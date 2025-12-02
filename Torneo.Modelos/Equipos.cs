ca941471f16723f9cc4bcf54327df0d9cbc8b6d0
Enma <c.enma69@gmail.com>
2025-12-02 14:39:02 -0500

    prueba e7

--- Torneo.Modelos/Equipos.cs
+++ Torneo.Modelos/Equipos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,11 @@ namespace Torneo.Modelos
 {
     public class Equipos
     {
-        public int Id { get; set; }
+        [Key] public int Id { get; set; }
         public string Nombre { get; set; }
-
-        // Navegación
-        public ICollection<Jugador> Jugadores { get; set; }
-        public ICollection<Torneo> Torneo { get; set; }
+                           
+        public ICollection<Jugador> Jugador { get; set; }
+               
+        public ICollection<TorneoEquipo> Torneo { get; set; }
     }
 }
