9e4396785243132ac657320647bb3be2839727d0
FERNANDO ISRAEL LOPEZ BURBANO <filopezb@utn.edu.ec>
2025-12-02 12:33:37 -0500

    branch

---
+++ Torneo.Modelos/Jugador.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torneo.Modelos
+{
+    internal class Jugador
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int EquipoId { get; set; }
+
+        // Propiedad de navegación
+        public Equipos Equipo { get; set; }
+        public int Goles { get; set; }
+        public int TarjetasAmarillas { get; set; }
+        public int TarjetasRojas { get; set; }
+        public int PartidosSuspendido { get; set; }
+    }
+}
